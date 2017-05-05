20bf4a5feb25cf6a39d28be60de1b2a59fccde31
a12462004 <a12462004@gmail.com>
2017-05-04 23:34:29 -0400

    finish edit book feature

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,6 +18,18 @@ class BooksController < ApplicationController
     #redirect_to new_candidate_path
   end
   end
+  def edit
+    @book = Book.find_by(id: params[:id])
+    redirect_to books_path, notice: "no data!" if @book.nil?
+  end
+  def update
+    @book = Book.find_by(id: params[:id])
+    if  @book.update(book_params)
+      redirect_to books_path, notice: "update"
+  else
+    render 'update'
+  end
+  end
   def book_params
     params.require("book").permit(:name, :introduction, :writer, :price)
   end
